4c74353b8c6249c2af80ebe8a418034f14ae7fda
Daniel Reszka <draens@hotmail.com>
2012-03-03 18:32:26 +0100

    images, dragonfly: added dragonfly columns to images (imagefile_uid, imagefile_name)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120303165908) do
+ActiveRecord::Schema.define(:version => 20120303172449) do
 
   create_table "albums", :force => true do |t|
     t.string   "title"
@@ -20,11 +20,12 @@ ActiveRecord::Schema.define(:version => 20120303165908) do
   end
 
   create_table "images", :force => true do |t|
-    t.string   "location"
+    t.string   "imagefile_uid"
     t.string   "name"
     t.integer  "album_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",     :null => false
+    t.datetime "updated_at",     :null => false
+    t.string   "imagefile_name"
   end
 
 end
