bb524b85271b785a4c0d24620862a1de68560a8d
Daniel Reszka <draens@hotmail.com>
2012-03-03 22:05:51 +0100

    images_form: created a simple_form for uploading images, using dragonfly

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,2 +1,15 @@
 class ImagesController < ApplicationController
+  def new
+    @image ||= Image.new
+    @albums = Album.all
+  end
+  def create
+    @image = Image.new(params[:image])
+    if @image.save
+      redirect_to album_path(@image.album)
+    else
+      new
+      render :new
+    end
+  end
 end
