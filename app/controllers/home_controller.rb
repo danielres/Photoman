1ff77921a1fd4526a8665e1443a9a240decc6fb5
Daniel Reszka <draens@hotmail.com>
2012-03-04 04:12:52 +0100

    dynamic_albums: user can now choose to display images by  albums or by tags

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,13 +4,10 @@ class HomeController < ApplicationController
   def index
     @albums = Album.all
 
-    # @files = Dir.glob("albums/*")
-
-    # @dirs = []
-    # Find.find('./albums/') do |f| @dirs << f end
-    # render :text => @dirs
-
     thumb_size = 100
+
+    @years = Dir['albums/*/']
+
     image_paths = Dir['albums/**/*.JPG'] + Dir['albums/**/*.jpg']
     @images = []
     image_paths.each{ |path|
@@ -23,36 +20,31 @@ class HomeController < ApplicationController
       meta[:width]      = e.width
       meta[:height]     = e.height
       meta[:tags]       = xmp.dc.subject rescue []
-      @images << { :path => path, :url => url, :meta => meta }
+      meta[:rating]     = xmp.xmp.send('Rating').to_i rescue nil
+      year              = path.split('/')[1]
+      album             = path.split('/')[2]
+      @images << {:year => year, :album => album, :path => path, :url => url, :meta => meta}
     }
 
-#    testimage = EXIFR::JPEG.new('albums/2002-09-19 16-07-31 moustique 03.JPG')
-#    xmp = XMP.parse(testimage)
+    @images_by_albums = {}
+    @images.each{ |i|
+      @images_by_albums[ i[:album] ]  ||= []
+      @images_by_albums[ i[:album] ]  <<  i
+    }
+
+    @images_by_tags = {}
+    @images.each{ |i|
+      i[:meta][:tags].each{ |t|
+      @images_by_tags[ t ]  ||= []
+      @images_by_tags[ t ]  <<  i
+      }
+    }
 
 
-#    attrs = []
-#    xmp.namespaces.each{ |ns|
-#      namespace = xmp.send(ns)
-#      namespace.attributes.each{ |attr|
-#        attrs << "#{ns}.#{attr}: " + namespace.send(attr).inspect
-#      }
-#    }
+    @grouping_list = ['albums', 'tags']
 
-
-
-
-
-
-    #    @thumbs = jpgs.map{ |i| File.new(i).path.split('/').insert(1, thumb_size).join('/') }
-
-    #    @thumbs = Hash[image_paths.map { |j|
-    ##        photo = Magick::Image.read(v).first
-    ##        date  = photo.get_exif_by_entry('DateTime')[0][1]
-    ##        date  = DateTime.strptime(date, exif_date_format) if date
-    ##        e = EXIFR::JPEG.new(v)
-    #        [j, j*2]
-    #    }.flatten]
-
+    @images = @images_by_albums
+    @images = @images_by_tags   if  params[:grouping] == 'tags'
 
   end
 end
