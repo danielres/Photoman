443e0efd376f517659612a05c1da11ac29a7515b
Daniel Reszka <draens@hotmail.com>
2012-03-12 11:53:37 +0100

    reduced model 'image' to a simple line with 'attr_accessor :imagefile'

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,8 +1,9 @@
-class Image < ActiveRecord::Base
-  belongs_to :album
-  image_accessor :imagefile
-
-  validates :album,     :presence => true
-  validates :imagefile, :presence => true
+class Image
+#  belongs_to :album
+#  image_accessor :imagefile
+#  attr_accessor :url
+  attr_accessor :imagefile
+#  validates :album,     :presence => true
+#  validates :imagefile, :presence => true
 
 end
